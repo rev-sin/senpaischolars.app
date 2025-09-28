e6fa8723666ef3bab6eb5b43f157d3cabe9da0cf
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-09-28 16:13:05 +0530

    add: next-themes and dotenv() package with custom prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,9 @@
 import path from "node:path";
 import type { PrismaConfig } from "prisma";
+import { config } from "dotenv";
+
+// Manually load environment variables from .env
+config();
 
 export default {
   schema: path.join("prisma"),
