f9bfcf7c42dae7647e0421de0eab1fe9f56a2b75
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-09-29 14:01:03 +0530

    chore: Biome formatting fixes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,36 +1,44 @@
-"use client"
+'use client';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { createClient } from '@/utils/supabase/component';
 
 export default function LoginPage() {
-  const router = useRouter()
-  const supabase = createClient()
+  const router = useRouter();
+  const supabase = createClient();
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   async function logIn() {
-    const { error } = await supabase.auth.signInWithPassword({ email, password })
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
     if (error) {
-      console.error(error)
+      console.error(error);
     }
-    router.push('/')
+    router.push('/');
   }
 
   async function signUp() {
-    const { error } = await supabase.auth.signUp({ email, password })
+    const { error } = await supabase.auth.signUp({ email, password });
     if (error) {
-      console.error(error)
+      console.error(error);
     }
-    router.push('/')
+    router.push('/');
   }
 
   return (
     <main>
       <form>
         <label htmlFor="email">Email:</label>
-        <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label htmlFor="password">Password:</label>
         <input
           id="password"
@@ -38,13 +46,19 @@ export default function LoginPage() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="button" onClick={logIn}>
+        <button
+          type="button"
+          onClick={logIn}
+        >
           Log in
         </button>
-        <button type="button" onClick={signUp}>
+        <button
+          type="button"
+          onClick={signUp}
+        >
           Sign up
         </button>
       </form>
     </main>
-  )
+  );
 }
